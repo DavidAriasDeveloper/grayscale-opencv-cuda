9beb699b4f37327b8fb88927d58c122f8fe3d498
David Arias <davidariasdeveloper@gmail.com>
2017-09-27 13:31:32 -0500

    Organizacion

--- preprocess.cpp
+++ preprocess.cpp
@@ -8,23 +8,23 @@
 
 using namespace std;
 
-cv::Mat imageRGBA;
-cv::Mat imageGrey;
+cv::Mat imgRGBA;
+cv::Mat imgGrey;
 
-uchar4        *d_rgbaImage__;
-unsigned char *d_greyImage__;
+uchar4        *d_rgbaImg__;
+unsigned char *d_greyImg__;
 
 size_t numRows() {
-  return imageRGBA.rows;
+  return imgRGBA.rows;
 }
 size_t numCols() {
-  return imageRGBA.cols;
+  return imgRGBA.cols;
 }
 // Devuelve un puntero de la version RGBA de la imagen de entrada
 // y un puntero a la imagend e canal unico de la salida
 // para ambos huesped y GPU
-void preProcess(uchar4 **inputImage, unsigned char **greyImage,
-                uchar4 **d_rgbaImage, unsigned char **d_greyImage,
+void preProcess(uchar4 **inputImg, unsigned char **greyImg,
+                uchar4 **d_rgbaImg, unsigned char **d_greyImg,
                 const string &filename) {
   //Comprobar que el contexto se inicializa bien
   checkCudaErrors(cudaFree(0));
@@ -34,26 +34,21 @@ void preProcess(uchar4 **inputImage, unsigned char **greyImage,
     cerr << "Couldn't open file: " << filename << endl;
     exit(1);
   }
-  cv::cvtColor(image, imageRGBA, CV_BGR2RGBA);
+  cv::cvtColor(image, imgRGBA, CV_BGR2RGBA);
 
- // Reserva memoria para el output
-  imageGrey.create(image.rows, image.cols, CV_8UC1);
-*inputImage = (uchar4 *)imageRGBA.ptr<unsigned char>(0);
-*greyImage  = imageGrey.ptr<unsigned char>(0);
-const size_t numPixels = numRows() * numCols();
+  // Reserva memoria para el output
+  imgGrey.create(image.rows, image.cols, CV_8UC1);
+  *inputImg = (uchar4 *)imgRGBA.ptr<unsigned char>(0);
+  *greyImg  = imgGrey.ptr<unsigned char>(0);
+  const size_t numPixels = numRows() * numCols();
   //Reserva memoria en el dispositivo
-  checkCudaErrors(
-cudaMalloc(d_rgbaImage, sizeof(uchar4) * numPixels));
-  checkCudaErrors(
-cudaMalloc(d_greyImage, sizeof(unsigned char) * numPixels));
-  checkCudaErrors(
-cudaMemset(*d_greyImage, 0, numPixels * sizeof(unsigned char)));
-// Asegurate de que no queda memoria sin liberar
-// Copia el input en la GPU
-  checkCudaErrors(
-cudaMemcpy(*d_rgbaImage, *inputImage,
- sizeof(uchar4) * numPixels, cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMalloc(d_rgbaImg, sizeof(uchar4) * numPixels));
+  checkCudaErrors(cudaMalloc(d_greyImg, sizeof(unsigned char) * numPixels));
+  checkCudaErrors(cudaMemset(*d_greyImg, 0, numPixels * sizeof(unsigned char)));
+  // Asegurate de que no queda memoria sin liberar
+  // Copia el input en la GPU
+  checkCudaErrors(cudaMemcpy(*d_rgbaImg, *inputImg,sizeof(uchar4) * numPixels, cudaMemcpyHostToDevice));
 
-  d_rgbaImage__ = *d_rgbaImage;
-  d_greyImage__ = *d_greyImage;
+  d_rgbaImg__ = *d_rgbaImg;
+  d_greyImg__ = *d_greyImg;
 }
