cf561990a95aa5a8132be5abca95263df4d955d0
David Arias <davidariasdeveloper@gmail.com>
2017-09-27 14:11:30 -0500

    Remove DisplayImage.cu

--- rgba_to_gray.cu
+++ rgba_to_gray.cu
@@ -5,7 +5,10 @@
 //Maximo de hilos por bloque
 #define TxB 1024
 
-__global__ rgba_to_grey_kernel(const uchar4* const rgbaImg, unsigned char* const greyImg, int rows, int cols){
+__global__ rgba_to_gray_kernel( const uchar4* const rgbaImg,
+                                unsigned char* const grayImg,
+                                int rows,
+                                int cols){
   /* El mapeo rgba en uchar4 es:
     .x -> R   .y -> G   .z -> B   .w -> A
 
@@ -14,15 +17,15 @@ __global__ rgba_to_grey_kernel(const uchar4* const rgbaImg, unsigned char* const
 
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   uchar4 pixel = rgbaImg[i]; //Pixel procesado por el hilo
-  greyImg[i] = (pixel.x + pixel.y + pixel.z)/3;
+  grayImg[i] = (pixel.x + pixel.y + pixel.z)/3;
 }
 
-void rgba_to_grey(uchar4* const d_rgbaImg, unsigned char* const d_greyImg, size_t rows, size_t cols){
+void rgba_to_gray(uchar4* const d_rgbaImg, unsigned char* const d_grayImg, size_t rows, size_t cols){
   long long int total_px = rows * cols;//Tamaño total de la imagen
   long int grids_n = ceil(total_px/TxB);//Redondeamos la cantidad grids
   const dim3 blockSize(TxB, 1, 1);
   const dim3 gridSize(grids_n, 1, 1);
-  rgba_to_grey_kernel<<gridSize,blockSize>>(d_rgbaImg, d_greyImg, rows, cols);
+  rgba_to_gray_kernel<<gridSize,blockSize>>(d_rgbaImg, d_grayImg, rows, cols);
   cudaDeviceSynchronize();
   checkCudaErrors(cudaGetLastError);
 }
